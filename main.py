ad8bffeeebd777262a2b6d16c33332cd9fc2dd65
Serhii Chervonospako <novikatmospro@gmail.com>
2024-01-29 20:04:16 +0100

    Lesson

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-New_dict = {
-    'Age': 24,
-    'Name': 'Serhii',
-    'koles': [1,2,3]     
+# coin = {
+#     'Anna': 85,
+#     'Petr': 92,
+#     'Mari': 78,
+#     'Ivan': 88
+# }
+# coin['Natali'] = 90
+
+# for a, b in coin.items():
+#     print(f'У {a} {b} монет')
+
+product = {
+    'Apple': 2.5,
+    'Milk': 1.0,
+    'Bread': 0.8,
+    'Eggs': 1.5,
 }
-Do_dict = {
-    'city':'Poland',
-    'name':'Karina',
-    'Name':'SERHII'
+magazine = {
+    'Apple': 3,
+    'Bread': 1,
+    'Eggs': 6,
 }
-Falst_dict = {
-    'Praca':'Elekrtomonter',
-    'city':'Irland',
-    'Age': 3
-}
-Fol_start = 
-print(f'{Fol_start}')
+total_cost = sum(product[key]* value for key, value in magazine.items())
+# print(f'Всего потрачено {total_cost}')
+
+print(f'{product.items()}')
