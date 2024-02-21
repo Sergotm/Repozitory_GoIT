7a901f7b299766d2db92e07266b4d0564beab8fd
Serhii Chervonospako <novikatmospro@gmail.com>
2024-02-21 20:09:50 +0100

    .

--- Python_Core/Module/Testing_time.py
+++ Python_Core/Module/Testing_time.py
@@ -1,5 +1,17 @@
-one_line_text = "Textual data in Python is handled with str objects, or strings. \
-Strings are immutable sequences of Unicode code points. \
-String literals are written in a variety of ways: \
-single quotes, double quotes, triple quoted."
-print(f'{one_line_text}')
+# url_search = '<https://www.google.com/search?q=Cat+and+dog&ie=utf-8&oe=utf-8&aq=t>'
+# _, jet = url_search.split('?')
+
+# obj_jet = {}
+# for el in jet.split('&'):
+#     key,values = el.split('=')
+#     obj_jet.update({key:values.replace('+',' ')})
+
+# for key, values in obj_jet.items():
+#     print(f'Key: {key} | Values: {values}')
+
+# user_input = input(f'Please write number: \n')
+# for i in user_input:
+#     if i.isdigit():
+#         print(f'Это число')
+#     else:
+#         print(f'Это не число')
