dd229346de8bd026c405e977f65f621705d20ca7
Serhii Chervonospako <novikatmospro@gmail.com>
2024-03-16 20:34:29 +0100

    .

--- Python_Core/venv.py
+++ Python_Core/venv.py
@@ -1,6 +1,8 @@
 'python -m venv .venv соззать новое Вир.Среду'
-'.\.venv\Scripts\Activate.ps1  Открыть ее Win' 
+'.\.venv\Scripts\Activate.ps1  Открыть ее Win PowerShell'
+'.\.venv\Scripts\activate.bat CMD'
 'source .venv/bin/activate macOS'
 'pip freeze > requirements.txt создать файл с '
+'pip install -r requirements.txt распаковка ВС'
 'pip freeze | % {pip uninstall -y $_.split('==')[0]}' 'Удалинеие всех пакетов pip'
 
